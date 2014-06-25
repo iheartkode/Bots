c67b18977381042cc3840d50021198fdaaaeee89
Mark <y2kmark73@gmail.com>
2014-06-25 14:51:35 -0700

    Update Bot.rb
    Re-factored the style

--- Bot.rb
+++ Bot.rb
@@ -1,51 +1,55 @@
  require 'sqlite3'
-  #Open the SQLite3 Database
-@db = SQLite3::Database.open "data.db"
 
-#Getting information to create a bot with
+  # Code to open the SQlite database.
+  @db = SQLite3::Database.open "data.db"
+
+
+  # Method used to start the information gathering.
   def start
     puts "Enter a Bots name:"
-
     @name = gets.chomp
-
     puts "Enter the age:"
-
     @age = gets.chomp
-
     puts "Enter a job:"
-
     @job = gets.chomp
-end
-#Inserting data in to data.db
+  end
+
+
+  # Method used to insert the data into SQlite.
   def insert
     {
       "name:" => @name,
       "age:" => @age,
       "job:" => @job
     }.each do |pair|
-    @db.execute "insert into bots values ( ?, ?,? )", pair
+      @db.execute "insert into bots values ( ?, ?,? )", pair
+    end
   end
-end
-#Showing inserted bots data and asks to show all bots.
+
+
+  # Method used to put info on the created bot and asks to show all bots.
   def view
     puts "Your bot has been added..."
-    puts "The name is #{@name} and his age is #{@age} and his job is #{@job}"
+    puts "The name is #{@name} and the age is #{@age} and the job is #{@job}"
     sleep 3
     puts "Would you like to list all the bots?"
     answer = gets.chomp.downcase
-#Asking if they want to see all the bots.
+
     if answer == "yes"
       @db.execute( "select * from bots" ) do |row|
-    puts  row 
+      puts  row 
     end
   else
     puts "Goodbye!"
   end
-#Close the connection
-@db.close()
 
+
+  # Close the SQlite data connection.
+  @db.close()
 end
 
-start()
-insert()
-view()
+
+# Start the sections of the program.
+start
+insert
+view
